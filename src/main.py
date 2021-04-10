2d7defc1879b679f384fa86c1caa2a4f00abe5c5
WouterSchols <wouter.schols@hotmail.com>
2021-04-10 16:37:11 +0200

    implemented hard i2c reset

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
 from src.Clients import EngineClient, ConsoleClient, ChessDotComClient
 import chess
-from typing import List, Any
+from typing import List, Any, Callable
 import setuptools
 
+class A:
+    val = 1
+
+class B:
+    a = A()
+
+def set(a, val):
+    a.val = val
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    white = ConsoleClient.ConsoleClient()
-    black = ChessDotComClient.ChessDotComClient(chess.BLACK)
-    while True:
-        black.set_move(white.get_move())
-        white.set_move(black.get_move())
+    # white = ConsoleClient.ConsoleClient()
+    # black = ChessDotComClient.ChessDotComClient(chess.BLACK)
+    # while True:
+    #     black.set_move(white.get_move())
+    #     white.set_move(black.get_move())
+    b = B()
+    print(b.a.val)
+    set(b.a, 2)
+    print(b.a.val)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
